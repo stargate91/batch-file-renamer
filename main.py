8eba702cc945255ce6f3619f66a975a77458053f
stargate91 <leventegallx@gmail.com>
2025-07-10 04:51:43 +0200

    Restructured modules

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 from variables import def_variables
 from validators import check_folder, check_prefix
-from operations import rename_files
-from dry import dry_rename_files
+from operations import rename_files, dry_rename_files
 
 def main():
     use_dry_run, folder_path, extension, prefix, use_zero_fill, files = def_variables()
--- operations.py
+++ operations.py
@@ -1,5 +1,5 @@
 import os
-from validators import check_files, check_file_exists
+from validators import check_files, check_file_exists, dry_check_file_exists
 
 def rename_files(files, extension, use_zero_fill, prefix, folder_path):
     index = 1
@@ -22,6 +22,31 @@ def rename_files(files, extension, use_zero_fill, prefix, folder_path):
             continue
         os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, newname))
         print(f"Renaming {filename} → {newname}")
+        index += 1
+
+    if not check_files(no_match_found, folder_path):
+        return
+
+def dry_rename_files(files, extension, use_zero_fill, prefix, folder_path):
+    index = 1
+    no_match_found = True
+
+    for filename in sorted(files):
+        matched_ext = next(
+        (ext for ext in extension if filename.lower().endswith(ext.lower())),
+        None
+        )
+        if matched_ext is None:
+            continue
+        no_match_found = False
+        clean_ext = matched_ext.lstrip(".")
+        if use_zero_fill > 0:
+            newname = f"{prefix}{index:0{use_zero_fill}}.{clean_ext}"
+        else:
+            newname = f"{prefix}{index}.{clean_ext}"
+        if not dry_check_file_exists(folder_path, filename, newname):
+            continue    
+        print(f"[DRY RUN] Would rename {filename} → {newname}")
         index += 1
 
     if not check_files(no_match_found, folder_path):
